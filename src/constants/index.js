ca3bfb43500d08c192cfdeef1f0ead69c2edbc5f
ArturSpirenkov <arturspirenkov@gmail.com>
2023-10-19 16:21:52 +0300

    feat: add constants

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+export const Tab = {
+  Profile: 1,
+  Search: 2,
+};
+
+export const SizePadding = {
+  md: '20',
+  lg: '40',
+};
+
+export const MaxWidth = {
+  sm: '600',
+  md: '1024',
+  lg: '1440',
+};
