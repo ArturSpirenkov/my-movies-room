de017af3235627509d78f5aa2421861473ce8431
ArturSpirenkov <arturspirenkov@gmail.com>
2023-10-19 23:58:30 +0300

    feat: add useAuthStore in index stores

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,5 +1,6 @@
 import { useMovieStore } from './MovieStore.js';
 import { useSearchStore } from './SearchStore.js';
 import { useThemeStore } from './ThemeStore.js';
+import { useAuthStore } from './AuthStore.js';
 
-export { useMovieStore, useSearchStore, useThemeStore };
+export { useMovieStore, useSearchStore, useThemeStore, useAuthStore };
