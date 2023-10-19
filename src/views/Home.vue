e48d20ca366df3c3dedc25ba234f07b9579881cb
ArturSpirenkov <arturspirenkov@gmail.com>
2023-10-19 16:23:23 +0300

    feat: add views

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Movie from '../components/Movie.vue';
+import Container from '../components/Container.vue';
+import Dashboard from '../components/Dashboard.vue';
+import { useMovieStore } from '@/stores';
+
+const movieStore = useMovieStore();
+</script>
+
+<template>
+  <Page>
+    <Container>
+      <div class="movies-list">
+        <h1>All Movies</h1>
+        <Dashboard />
+        <Movie
+          v-for="movie of movieStore.movies"
+          :key="movie.filmId"
+          :movie="movie"
+          class="movies-list__item"
+        />
+      </div>
+    </Container>
+  </Page>
+</template>
+
+<style lang="scss" scoped>
+.movies-list {
+  &__item {
+    margin-bottom: 10px;
+  }
+}
+</style>
---
+++ src/views/SearchMovies.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Search from '../components/Search.vue';
+import Container from '../components/Container.vue';
+</script>
+
+<template>
+  <div>
+    <Page>
+      <Container>
+        <Search />
+      </Container>
+    </Page>
+  </div>
+</template>
